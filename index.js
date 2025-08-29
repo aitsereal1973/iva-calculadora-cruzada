9325e9fced49c8ded022db990cc5c9710048c523
aitsereal1973 <odidoquinom@hotmail.com>
2025-08-28 20:43:31 -0500

    Add comments to index.js for better clarity

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+// Importar AppRegistry de react-native para registrar la aplicación
 import { AppRegistry } from 'react-native';
+// Importar el componente principal de la aplicación
 import App from './src/App';
+// Importar el nombre de la aplicación desde app.json
 import { name as appName } from './app.json';
 
+// Registrar el componente principal con AppRegistry
 AppRegistry.registerComponent(appName, () => App);
